adf051875bddcb9a4e38f4df2a84015532cf6caf
Entoma1824 <68795418+Entoma1824@users.noreply.github.com>
2023-06-14 17:05:48 +0900

    2 files modified, 5 added

--- scripts/02-lesson-excercises.js
+++ scripts/02-lesson-excercises.js
@@ -39,6 +39,7 @@ function fahrenheitToCelsius(fahrenheit) {
   const celsius = (fahrenheit -32) *(5/9)
   return `${celsius}°C`;
 }
+
 function celsiusToFahrenheit(celsius) {
   const fahrenheit = (celsius *(9/5)) +32;
   return `${fahrenheit}°F`;
---
+++ scripts/03-lesson-excercises.js
@@ -0,0 +1,7 @@
+// https://cdn.discordapp.com/attachments/1118124058417057904/1118403128962846720/image.png
+
+//https://cdn.discordapp.com/attachments/1118124058417057904/1118406047430889614/image.png
+
+//https://supersimple.dev/projects/amazon/checkout
+
+//https://cdn.discordapp.com/attachments/1118124058417057904/1118421364408324116/image.png
