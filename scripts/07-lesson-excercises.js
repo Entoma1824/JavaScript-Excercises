5d7fa56ab5e8d442ddb3a2beea32dd6318f1e220
Entoma1824 <68795418+Entoma1824@users.noreply.github.com>
2023-06-19 00:20:22 +0900

    2 files added

---
+++ scripts/07-lesson-excercises.js
@@ -0,0 +1,35 @@
+//7a & 7b
+function greet(name) {
+  //7c
+  if(name !== undefined) {
+    console.log(`Hello ${name}`);
+  } else {
+    console.log('Hello there!');
+  } 
+}
+
+//7d
+function convertToFahrenheit(celsius) {
+  const fahrenheit = (celsisus *(9/5)) +32;
+
+  return fahrenheit;
+}
+
+//7e
+function convertToCelsius(fahrenheit) {
+  const celsius = (fahrenheit - 32) *(5/9)
+
+  return celsius;
+}
+//7f
+function converTemperature(degrees, unit) {
+  if(!unit) {
+    if(unit === ('F'|| 'f')) {
+      const celsius = convertToCelsius(degrees);
+      console.log(`${celsius}C`);
+    } else if(unit === ('C'|| 'c')) {
+      const fahrenheit = convertToFahrenheit(degrees);
+      console.log(`${fahrenheit}F`);
+    }
+  } 
+}
