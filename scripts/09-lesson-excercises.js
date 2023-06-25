9b4efca30ca7ab92a20360ac8c98ea7f92907ea6
Entoma-Von-Degurechaff <68795418+Entoma-Von-Degurechaff@users.noreply.github.com>
2023-06-25 18:32:44 +0900

    3 files modified

--- scripts/09-lesson-excercises.js
+++ scripts/09-lesson-excercises.js
@@ -1,2 +1,20 @@
 // 9a
 console.log(document.querySelector('button'));
+
+//9b
+document.querySelector('.js-button').innerHTML = '9b done!';
+
+//9c
+function playGame(playerGuess) {
+    document.querySelector('.score-board').innerHTML = `You chose: ${playerGuess}`;
+}
+// 9d and 9e and 9f
+function getText() {
+    const inputName = document.getElementById("input-text");
+    const getName = inputName.value;
+
+    const output = 'Your name: ' + getName;
+
+    const outputName = document.querySelector('.name-display');
+    outputName.innerText = output;
+}
