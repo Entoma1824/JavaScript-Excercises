21264501ec344f8d9f88ccfdd7e0746ab101e828
Entoma-Von-Degurechaff <68795418+Entoma-Von-Degurechaff@users.noreply.github.com>
2023-06-30 18:58:15 +0900

    3 files modified and it is still not yet finished

--- scripts/10-calculator-final.js
+++ scripts/10-calculator-final.js
@@ -1,39 +1,63 @@
-
 let calculation = localStorage.getItem('calculation') || '';
 document.getElementById('result').value = calculation;
 
 function appendToResult(value) {
-  calculation += value;
-  const mathOperatorsClasses = document.querySelector('.math-operators');
-  // if(!(value === '-')) {
+  if(!(value === '-1*')) {
+    calculation += value;
     document.getElementById('result').value += value;
     localStorage.setItem('calculation', value)
     console.log(calculation);
-  // } else {
-//     if(value === '-') {
-//       document.getElementById('result').value = value + document.getElementById('result').value;
-//       localStorage.setItem('calculation', value)
-//       console.log(calculation);
-// //     } else {
-// // //      const negativeToPositive = value.replace()
+    
+    // if(value === '%') {
+    //   // const removePercent = value.substring(0, value.length - 1);
+    //   const percentValue = removePercent+'/100';
+  
+    //   document.getElementById('result').value = percentValue; 
+    //   calculate();
+    // }
 
-// //       document.getElementById('result').value += value;
-// //       localStorage.setItem('calculation', value)
-// //       console.log(calculation);
-// //     }
-//   }
+  }
+  if(value === '-1*') {
+    calculation += value;
+    document.getElementById('result').value = value + document.getElementById('result').value ;
+    calculate();
+    console.log(calculation);
+  } 
+  // const operatorsClass = 
+  if(document.querySelector('.math-operators').classList.contains('.toggle-math-operators')){
+    mathOperatorsClass.classList.remove('toggle-math-operators');
+  }
 }
 
+
+function mathOperators(operator, className) {
+  calculation += operator;
+  const mathOperatorsClass = document.querySelector(`.${className}`);
+
+  if(!(mathOperatorsClass.classList.contains('.toggle-math-operators'))) {
+    mathOperatorsClass.classList.add('toggle-math-operators');
+  } else if(mathOperatorsClass.classList.contains('.toggle-math-operators')){
+    mathOperatorsClass.classList.remove('toggle-math-operators');
+  }
+  localStorage.setItem('calculation', operator)
+}
+
+
+
 function calculate() {
-  const result = eval(document.getElementById('result').value);
+  const result = eval(calculation);
   document.getElementById('result').value = result;
-  // localStorage.getItem('calculation')
-  localStorage.setItem('calculation', result)
+  localStorage.setItem('calculation', result);
   console.log(result);
 }
 
+
+
 function clearResult() {
+  calculation = '';
   document.getElementById('result').value = '';
   localStorage.removeItem('calculation');
   console.log(calculation)
 }
+console.log(document.getElementById('result').value)
+console.log(calculation);
